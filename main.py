099ff0ef6e548475a6a86727fe1bc580afe79a68
mink0426 <127376406+mink0426@users.noreply.github.com>
2023-03-09 21:42:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-nums = []
 def twoSum(nums, target)
-  for i in range(0, len(nums)):
-    for j in range(i+1, len(nums)):
-	    if nums[i] + nums[j] == target:
-	      return [i, j]
+	nums = [*map(int,input().split())]
+	
+
+
+return None
