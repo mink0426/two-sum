2ee95b5bc3863b52b677b783ba4b7d8cd5d62454
mink0426 <mink0426@g.skku.edu>
2023-06-07 15:02:34 +0000

    complete code

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def twoSum(nums, target)
-	if i in nums + j in nums == target
-	print(nums[i], nums[j])
-return None
+def twoSum(target, len, nums=[]):
+    for i in range(0,len-1,1):
+        for j in range(i+1, len, 1):
+            if(nums[i]+nums[j]==target):
+                result_array = [i, j]
+                return result_array
+
+nums = [2,7,11,15]
+result = twoSum(9,len(nums),nums)
+print(result)
